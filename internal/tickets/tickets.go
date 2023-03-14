72109ee139e7574f694a8b96081f275bbc66df43
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-03-13 21:46:58 -0500

    doc: add global vars description

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -77,12 +77,11 @@ var (
 	// every ticket which flightTime is in the range of `07:00` and `012:59`
 	Morning 		= Period{ParseToFlightTime("07:00"), ParseToFlightTime("12:59")}
 
-	// EearlyMorning represents a early morning `Period`, this is use to match
+	// Afternoon represents a afternoon `Period`, this is use to match
 	// every ticket which flightTime is in the range of `13:00` and `19:59`
 	Afternoon 		= Period{ParseToFlightTime("13:00"), ParseToFlightTime("19:59")}
 
-
-	// EearlyMorning represents a early morning `Period`, this is use to match
+	// Evening represents a evening `Period`, this is use to match
 	// every ticket which flightTime is in the range of `20:00` and `23:59`
 	Evening 		= Period{ParseToFlightTime("20:00"), ParseToFlightTime("23:59")}
 )
