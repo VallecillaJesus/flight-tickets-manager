6446c7fe9476ba457210dca8a15b5c18e3612c4f
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-03-15 22:10:56 -0500

    feat: add ParseToFlightTime function test

--- internal/tickets/tickets_test.go
+++ internal/tickets/tickets_test.go
@@ -11,3 +11,24 @@ func TestReadTickets(t *testing.T) {
 	assert.NotNil(t, tickets)
 	assert.EqualValues(t, 1000, len(tickets))
 }
+
+
+func TestParseToFlightTimeInvalidFormat(t *testing.T) {
+	defer func() {
+		r := recover()
+		assert.NotNil(t, r, "The function did not panic")
+	}()
+	// Invalid time string format
+	ParseToFlightTime("0000")
+}
+
+func TestParseToFlightTime(t *testing.T) {
+
+	parsedTime := ParseToFlightTime("00:00")
+	assert.NotEmpty(t, parsedTime, "The parsed time returned is empty")
+
+	parsedTime = ParseToFlightTime("23:59")
+	assert.NotEmpty(t, parsedTime, "The parsed time returned is empty")
+}
+
+
